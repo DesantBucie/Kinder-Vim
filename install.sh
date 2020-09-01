d6fa70a9ce29fef0965565cae862bc4d4a41810c
DesantBucie <mattrusz@gmail.com>
2020-09-01 09:34:51 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
-mv ~/.vim ~/.vimbackup
-mv ~/.vimrc ~/.vimrcbackup
+VIMRC=~/.vimrc
+VIM=~/.vim
+if [-d "$VIM" ]; then
+    mv ~/.vim ~/.vim_backup
+fi
+
+if [-f "VIMRC"]; then
+    mv ~/.vimrc ~/.vimrc_backup
+fi
 
 cp .vimrc ~/
 cp -r .vim ~/
