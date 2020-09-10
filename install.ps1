9855b33cf8e517b58ecaa1acf3a386a902b74b9b
DesantBucie <mattrusz@gmail.com>
2020-09-10 21:03:02 +0200

    Test of ps1. script

--- install.ps1
+++ install.ps1
@@ -1,1 +1,31 @@
-[System.IO.File]::Exists($path)
+$vimrc = "~\_vimrc"
+$vim = "~\_vim"
+
+if (Test-Path $vimrc -PathType leaf) {
+    Rename-Item -Path $vimrc -NewName "_vimrc_backup"
+}
+
+if (Test-Path $vim -PathType leaf) {
+    Rename-Item -Path $vim -NewName "_vim_backup"
+}
+Rename-Item -Path ".vim" -NewName "_vim"
+Rename-Item -Path ".vimrc" -NewName "_vimrc"
+
+Copy-Item _vim ~\
+
+if (Test-Path "coc-settings.json" -PathType leaf) {
+    Copy-Item coc-settings.json ~\_vim\
+}
+else {
+    Get-Content _vimrc | 
+        Where-Object ReadCount -ne 54 |
+            Set-Content -Encoding Utf8 _vimrc.temp
+            Move-Item -Force _vimrc.temp _vimrc
+}
+Copy-Item _vimrc ~\
+
+vim -c "PlugInstall|q|q"
+
+vim -c "CocInstall coc-css coc-cssmodules coc-stylelintplus coc-tsserver coc-json coc-html coc-discord coc-emmet coc-todolist coc-template |q|q"
+
+
