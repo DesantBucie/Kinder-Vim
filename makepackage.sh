af15428d3c081d692a6eac086033577bbea8ee38
DesantBucie <mattrusz@gmail.com>
2020-10-10 00:01:10 +0200

    Modified to automaticaly make packages

--- makepackage.sh
+++ makepackage.sh
@@ -1,10 +1,41 @@
 RELEASE=release
 
 if [ -d "$RELEASE" ];then
-    echo "release directory found"
+    if [ -d release.bak ];then
+        rm -rf release.bak
+        mv release release.bak
+        mkdir release
+    else
+        mv release release.bak
+        mkdir release
+    fi
 else
     mkdir release
 fi
 
-cp -r .vimrc .vim install.sh uninstall.sh release/
+cd release
+
+mkdir Verde
+
+cd ..
+
+cp -r .vimrc .vim install.sh uninstall.sh release/Verde/
+
+cd release
+
+zip -vr Verde.zip Verde/ -x "*.DS_Store"
+
+rm -rf Verde 
+
+mkdir VerdeMini
+
+cd ..
+
+cp .vimrc_mini release/VerdeMini
+
+cd release
+
+zip -vr VerdeMini.zip VerdeMini/ -x "*.DS_Store"
+
+rm -rf VerdeMini
 
